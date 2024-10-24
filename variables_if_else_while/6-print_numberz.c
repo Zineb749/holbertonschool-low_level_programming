5fcacf275dac51c93ebfa91e601e90bbd0d0abc8
Zineb749 <zsakho58@gmail.com>
2024-10-24 19:44:12 +0200

    numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	
+	char i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
