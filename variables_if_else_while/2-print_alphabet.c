0dd7d89a4eac331d31fcb070a0f0e84ec7a271fd
Zineb749 <zsakho58@gmail.com>
2024-10-24 10:29:45 +0200

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,13 @@
 int main(void)
 
 {
-        for (char c = 'a'; c <= 'z'; c++)
-        {
-                putchar(c);
+	for (char c = 'a'; c <= 'z'; c++)
 
-        }
+		putchar(c);
 
-        return (0);
+		putchar('\n');
 
 
+	return (0);
 }
 
