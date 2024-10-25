198c165631e9d60cc67b49cb0907f2dc04d21bef
Zineb749 <zsakho58@gmail.com>
2024-10-25 15:06:51 +0200

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include "main.h"
+void print_alphabet(void)
+{
+
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+
+
+
+}
