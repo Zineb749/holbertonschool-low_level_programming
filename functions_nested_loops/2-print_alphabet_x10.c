45eb4026d8d7f0bbb8aafd5ea832f39046c8d3ff
Zineb749 <zsakho58@gmail.com>
2024-10-25 16:01:05 +0200

    alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,13 @@ void print_alphabet_x10(void)
 	int c;
 	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i <= 10; i++)
 	{
 
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
 
-		for (i = 0; i <= 10; i++)
+			_putchar(c);
 
-			_putchar(i);
 	}
 	_putchar('\n');
 
