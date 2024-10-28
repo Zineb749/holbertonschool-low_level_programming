702d86042186f3e36f73048610eb9ef20802460e
Zineb749 <zsakho58@gmail.com>
2024-10-28 13:30:25 +0100

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-print_numbers.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - prints number.
+ *
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void print_numbers(void)
+
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+
+		_putchar(c);
+
+	_putchar('\n');
+
+
+}
