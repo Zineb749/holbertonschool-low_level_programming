301ca7abb6be6ede68448ce742a8583e540a82a6
Zineb749 <zsakho58@gmail.com>
2024-10-28 13:04:42 +0100

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: integer to multiply.
+ * @b: integer to multiply.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int mul(int a, int b)
+{
+
+
+	return (a * b);
+
+
+}
