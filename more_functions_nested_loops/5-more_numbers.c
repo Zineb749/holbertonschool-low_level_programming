b026307bdae53465a0cf2929f7a7fbbe877604b9
Zineb749 <zsakho58@gmail.com>
2024-10-29 10:50:31 +0100

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -20,9 +20,9 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 
-			if (c >= 10)
+		_putchar((c / 10) + '0');
 
-			_putchar((c  / 10) + '0');
+		 _putchar((c % 10) + '0');
 	}
 	_putchar('\n');
 
