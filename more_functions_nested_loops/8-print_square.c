8ba702fd1a19c7ce20bf3c647a5bcdcff8f32812
Zineb749 <zsakho58@gmail.com>
2024-10-29 13:35:11 +0100

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+/*
+ * File: 8-print_triangle.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_square - prints a triangle.
+ *@n : character to be checked
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void print_square(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('#');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+
+}
