e4065d2939bac07e38a0b5bf22f1633d12c2fb10
Zineb749 <zsakho58@gmail.com>
2024-10-29 11:33:59 +0100

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+/*
+ * File: 6-print_line.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_line - prints line.
+ *@n: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void print_line(int n)
+{
+
+	int i = 0;
+
+	if (i > 0)
+
+		_putchar('_');
+
+	if (i < 0)
+
+		_putchar('\n');
+
+
+
+}
