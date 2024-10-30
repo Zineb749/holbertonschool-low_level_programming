b63902af38d470cc9d518259a8c6ad4cc93b4bf1
Zineb749 <zsakho58@gmail.com>
2024-10-30 11:52:03 +0100

    Reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-reset_to_98.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - Checks if a character is uppercasee.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+
+
+}
