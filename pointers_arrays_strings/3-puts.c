9bb95e665a230cf5bb00bd8e21ccc911af091655
Zineb749 <zsakho58@gmail.com>
2024-10-30 13:38:21 +0100

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ * @str: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void _puts(char *str)
+{
+	while (*str)
+
+	{ _putchar(*str++);
+
+	}
+
+	_putchar('\n');
+
+
+
+
+}
