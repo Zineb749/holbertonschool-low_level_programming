448d6c81c3e92e9779a84e287fc102e6b73cb6e7
Zineb749 <zsakho58@gmail.com>
2024-10-30 23:20:29 +0100

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ * @s: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void print_rev(char *s)
+{
+	while (*s)
+
+	{ _putchar(*s--);
+
+	}
+
+	_putchar('\n');
+
+
+
+
+}
