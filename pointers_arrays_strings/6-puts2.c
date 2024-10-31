8d891af9bbff1421947fbb90619deb35cc745952
Zineb749 <zsakho58@gmail.com>
2024-10-31 11:49:43 +0100

    half string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in te stirng.
+ * @str: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+void puts2(char *str)
+{
+	while (*str)
+
+	{ _putchar(*str / 2);
+
+	}
+
+	_putchar('\n');
+
+
+
+
+}
