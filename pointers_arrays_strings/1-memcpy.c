5efe001e5e8771437f85876db1d38a2e4e46fb18
Zineb749 <zsakho58@gmail.com>
2024-11-05 10:52:38 +0100

    memcopy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-memcpy.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory.
+ * @src: The character to be checked.
+ * @dest: The character to be checked.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
