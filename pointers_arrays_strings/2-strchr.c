91e13ecdb8ab6fcc7c372061f772ce125c84fe92
Zineb749 <zsakho58@gmail.com>
2024-11-05 11:49:15 +0100

    search

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * _strchr - Checks if a character is uppercasee.
+ * @c: The character to be checked.
+ * @s: the character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+
+char *_strchr(char *s, char c)
+{
+		while (*s)
+	{
+		if (*s == c)
+
+		return (s);
+
+		s++;
+	}
+
+	return (0);
+
+}
+
