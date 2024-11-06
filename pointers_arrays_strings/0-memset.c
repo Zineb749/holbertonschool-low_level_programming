2ee65e67292b27748425712ae2ea5b6eabdf3624
Zineb749 <zsakho58@gmail.com>
2024-11-06 11:13:49 +0100

    Redo the set memory task

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,25 +1,18 @@
-/*
- * File: 0-memset.c
- *
- */
-
 #include "main.h"
 
 /**
- * _memset - Checks if a character is uppercasee.
- * @s: The character to be checked.
- * @b: The character to be checked.
- * @n: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * *_memset - Function that fills the memory
+ * @s: The string
+ * @b: Character
+ * @n: the first n byte
+ * Return: A pointer to the memory area
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+		*(s + i) = b;
 
 	return (s);
-
 }
