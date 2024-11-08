778c421d97e5dfb425aae4eb105ceb4094da29c1
Zineb749 <zsakho58@gmail.com>
2024-11-08 13:35:36 +0100

    Args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc: character to be checked.
+ *@argv: character to be checked.
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+
+	printf("argc: %d\n", argc);
+
+	return (0);
+}
