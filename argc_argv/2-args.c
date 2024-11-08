71a1da0a12f47801c3f5c0202b569a8b1db32e9b
Zineb749 <zsakho58@gmail.com>
2024-11-08 13:51:37 +0100

    2args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	printf("%d \n", argc);
+
+	for (i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
