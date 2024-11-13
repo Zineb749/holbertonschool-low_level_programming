fe2eb4ed169587d6c905fd31416c132ed62b4494
Zineb749 <zsakho58@gmail.com>
2024-11-13 11:59:30 +0100

    create array

---
+++ malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _puts_recursion(char *s);
+int _putchar(int c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int helper_prime(int n, int i, int limit);
+int helper(int n, int i);
+char *create_array(unsigned int size, char c);
+
+
+#endif
