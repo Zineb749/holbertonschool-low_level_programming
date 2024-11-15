3896c4c63c821d5c4909e629b1e44b3182f770e2
Zineb749 <zsakho58@gmail.com>
2024-11-15 13:41:32 +0100

    dog age

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/*
+ * File: 0-isupper.c
+ *
+ */
+
+
+/**
+ * init_dog - Checks if a character is uppercasee.
+ * @d: The character to be checked.
+ * @name: The charcter to be checked.
+ * @age : age.
+ * @owner: owner.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
