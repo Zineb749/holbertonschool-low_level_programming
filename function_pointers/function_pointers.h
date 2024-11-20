4b33c72e22c2cf5bb7c889c899e3cee9dbfc4a32
Zineb749 <zsakho58@gmail.com>
2024-11-20 18:06:08 +0100

    header file fix

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,11 @@
+#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 
 #endif
