50817129813bad6c31ddfd8119f409b631fb222b
Zineb749 <zsakho58@gmail.com>
2024-11-22 21:25:33 +0100

    sum them all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,38 +1,31 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
-/**
- * sum_them_all- sum of all parameters.
- * @n: character to be checked
- *
- *
- * Return: Nothing.
- */
 
+/**
+  * sum_them_all - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, x;
-	int s = 0;
 	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
 
-	if (n == 0)
+	if (n != 0)
+	{
+		va_start(args, n);
 
-		return (0);
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
 
-	va_start(args, n);
-
-
-	for (i = 0; i < n; i++)
-
-
-	{ x = va_arg(args, int);
-		s += x;
+		va_end(args);
+		return (sum);
 	}
 
-	va_end(args);
-
-	return (s);
-
+	return (0);
 }
-
-
