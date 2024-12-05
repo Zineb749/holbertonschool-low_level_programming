5a752468ba2abc02aed8fda3c2cb8885d3fae12e
Zeynab <zsakho58@gmail.com>
2024-12-05 10:53:57 +0100

    betty fix

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * list_len - returns the elements of a linked list.
+ * @h: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 
 size_t list_len(const list_t *h)
 {
