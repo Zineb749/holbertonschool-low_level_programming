9f81066532e1a4b5562cc9ed92cde913ec1e7f23
Zeynab <zsakho58@gmail.com>
2024-12-05 01:49:10 +0100

    list length

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+
+	return (count);
+}
+
