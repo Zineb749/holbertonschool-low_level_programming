17592755e9180d25f7592bdfc4e1fef6e005d19d
Zeynab <zsakho58@gmail.com>
2024-12-05 01:23:12 +0100

    fix code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 
 size_t print_list(const list_t *h)
-{	
-	h = 0; 
+{
+	size_t count = 0;
 
-	if (h != NULL)
-		h++;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-	if (h == NULL)
-		printf("[0] (Nil)");	
-		
-	return (0);
+		h = h->next;
+		count++;
+	}
 
+	return (count);
 }
+
