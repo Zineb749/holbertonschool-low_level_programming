9acb330fe5ac01c527d2b67f866f4677178dd91b
Zeynab <zsakho58@gmail.com>
2024-12-05 01:11:02 +0100

    print elements

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+size_t print_list(const list_t *h)
+{	
+	h = 0; 
+
+	if (h != NULL)
+		h++;
+
+	if (h == NULL)
+		printf("[0] (Nil)");	
+		
+	return (0);
+
+}
